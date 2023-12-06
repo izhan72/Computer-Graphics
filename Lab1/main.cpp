f475e4832aa9648b4c396d2cfaf00c624ab83581
Izhan Siddiqui <123665945+izhan72@users.noreply.github.com>
2023-12-06 23:45:56 +0500

    Add files via upload

---
+++ Lab1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <GL/glut.h>
+
+int windowWidth = 1600;
+int windowHeight = 900;
+
+void display() 
+{
+	glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer
+	glClearColor(0.9f, 0.9f, 0.9f, 0.0); // Set clear color to black
+	glColor3f(1, 0, 0);
+	glFlush(); // Render the scene
+}
+
+int main(int argc, char** argv) 
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(windowWidth, windowHeight);
+	glutCreateWindow("Lab 01");
+	glutDisplayFunc(display); // Set the display callback function
+    glutMainLoop(); // Start the main GLUT loop
+	return 0;
+}
